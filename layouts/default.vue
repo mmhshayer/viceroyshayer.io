a0b38d00ad5b552bd619367c53da6d76b3004e52
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-11 15:28:13 +0600

    improved responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,15 @@
 <template>
   <body class="pt-20">
     <Global-Header class="fixed top-0 inset-x-0 z-50 h-20"/>
-    <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 ">
+    <div class="
+        mx-auto
+        px-4
+        sm:max-w-md
+        md:max-w-xl
+        lg:max-w-2xl
+        xl:max-w-4xl
+        2xl:max-w-7xl
+      ">
       <div class="mx-auto flex flex-col">
         <Nuxt class="min-h-screen mb-5"/>
         <Global-Footer />
@@ -18,11 +26,19 @@
      youtube =  https://www.youtube.com/watch?v=1hoSQhtd0IQ
     header is put inside a flex auto centering
     mb-auto @ "Nuxt" needs reasearch of purpose
-    max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0 makes shit responsive it needs improvement for all screen size
+
+                                    max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0
+
+    makes shit responsive it needs improvement for all screen size
     footer is flex-column auto centering
 
     flex flex-col
     grid grid-cols-1
+
+
+    Note 2 -
+              sm:max-w-md
+    seems to have no effect on phone so far, needs furthur research
   */
   html {
     font-family:
