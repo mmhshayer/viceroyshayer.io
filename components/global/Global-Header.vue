cafde5dd02bb76cf494acefe4192401f135cc33f
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-11 16:40:42 +0600

    ++ shadow on scroll

--- components/global/Global-Header.vue
+++ components/global/Global-Header.vue
@@ -1,12 +1,32 @@
 <template>
-  <header class="flex flex-row items-center justify-center">
+  <header class="flex flex-row items-center justify-center" :class="{ 'scrolled': !view.atTopOfPage }">
     <nuxt-link to="/">viceroyshayer</nuxt-link>
   </header>
 </template>
 
 <script>
   export default {
+    data () {
+        return {
+            view: {
+                atTopOfPage: true
+            }
+        }
+    },
 
+    beforeMount () {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+
+    methods: {
+        handleScroll(){
+            if(window.pageYOffset>0){
+                if(this.view.atTopOfPage) this.view.atTopOfPage = false
+            }else{
+                if(!this.view.atTopOfPage) this.view.atTopOfPage = true
+            }
+        }
+    }
   }
 </script>
 
@@ -14,4 +34,8 @@
   /*
   apply bg-red-700 at first div to see it in action
   */
+  .scrolled {
+    @apply shadow-xl;
+    border-bottom: 0px;
+  }
 </style>
