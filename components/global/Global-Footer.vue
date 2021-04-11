f395f7131748ccba6a9a631c758cafba2ffc7ca7
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-11 14:01:31 +0600

    footer; change style accordingly later

---
+++ components/global/Global-Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <div className="flex flex-col items-center">
+      <div className="flex mb-3 space-x-4">
+        <a href="#">enter icons here</a>
+        <a href="#">enter icons here</a>
+      </div>
+      <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <span>&middot;</span>
+        <span>Powered by <a href="https://nuxtjs.org/" rel="noopener noreferrer" target="_blank">NUXTJS</a></span>
+        <span>&middot;</span>
+        <span>Made by <a href="https://github.com/viceroyshayer" rel="noopener" target="_blank">viceroyshayer</a></span>
+      </div>
+      <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        <a href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+          Tailwind NUxtjs Theme
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+ /*
+ apply bg-pink-500 at footer to see it
+ */
+</style>
