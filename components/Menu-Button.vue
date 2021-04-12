3cac8bb19890138be12591da693f93d45a61b796
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-12 15:46:28 +0600

    ++ analyzer; ++ button ber testing

---
+++ components/Menu-Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button> <img src="~assets/icons/instagram.svg" alt="button" class="w-10"> </button>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
