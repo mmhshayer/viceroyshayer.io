fd2fca5056886ea742dab5fbc8db40a26c3f91b5
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-15 13:53:47 +0600

    ++set footer

--- components/Global-Footer.vue
+++ components/Global-Footer.vue
@@ -1,19 +1,21 @@
 <template>
   <footer>
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center text-gray-500 dark:text-gray-400">
       <div class="flex mb-3 space-x-4">
-        <a href="#">enter icons here</a>
-        <a href="#">enter icons here</a>
+        <nuxt-link to="/about">About</nuxt-link>
+        <span> | </span>
+        <nuxt-link to="/contact">Contact</nuxt-link>
       </div>
-      <div class="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+      <div class="flex mb-2 space-x-2 text-sm ">
+        <span>Powered by <a href="https://nuxtjs.org/" rel="noopener" target="_blank">NUXTJS</a></span>
         <span>&middot;</span>
-        <span>Powered by <a href="https://nuxtjs.org/" rel="noopener noreferrer" target="_blank">NUXTJS</a></span>
+        <span> <a href="https://tailwindcss.com/" rel="noopener" target="_blank">TailwindCSS</a></span>
         <span>&middot;</span>
         <span>Made by <a href="https://github.com/viceroyshayer" rel="noopener" target="_blank">viceroyshayer</a></span>
       </div>
-      <div class="mb-8 text-sm text-gray-500 dark:text-gray-400">
+      <div class="mb-8 text-sm">
         <a href="https://github.com/viceroyshayer">
-          Tailwind NUxtjs Theme
+          © 2020 viceroyshayer
         </a>
       </div>
     </div>
@@ -27,7 +29,4 @@
 </script>
 
 <style scoped>
- /*
- apply bg-pink-500 at footer to see it
- */
 </style>
