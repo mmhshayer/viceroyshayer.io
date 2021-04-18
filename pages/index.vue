b716661216d60957e31c541fdc293ad6f84d2894
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-18 22:59:05 +0600

    ++carousel on Home

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="flex flex-col justify-center content-center text-center">
-    <div>
-      <h1>
-        viceroyshayer.github.io
-      </h1>
+  <div>
+    <Carousel />
+    <div class="flex flex-col justify-center content-center text-center min-h-screen">
       <div>
-        <nuxt-link to="/about">About</nuxt-link>
-        <nuxt-link to="/contact">Contact</nuxt-link>
-        <nuxt-link to="/showcase">showcase</nuxt-link>
-        <nuxt-link to="/blog">blog</nuxt-link>
+        <h1>
+          viceroyshayer.github.io
+        </h1>
+        <div>
+          <nuxt-link to="/about">About</nuxt-link>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+          <nuxt-link to="/showcase">showcase</nuxt-link>
+          <nuxt-link to="/blog">blog</nuxt-link>
+        </div>
       </div>
     </div>
   </div>
