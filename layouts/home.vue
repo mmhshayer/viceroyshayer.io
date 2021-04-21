da4914f9e8f46b853ed6dd2e0b3eb94fe447fbfc
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-21 19:13:02 +0600

    Front page Carousel

---
+++ layouts/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <body class="cbpt max-h-screen overflow-y-scroll snap snap-y snap-proximity snap-padding">
+    <Global-Header class="fixed top-0 z-50" />
+    <section class="w-full cbmh bg-red-200 snap-start">
+      Section 1
+    </section>
+    <Carousel class="snap-start"/>
+    <section class="w-full cbxh bg-green-200 snap-start">
+      Section 3
+    </section>
+    <section class="w-full cbxh bg-indigo-200 snap-start">
+      Section 4
+    </section>
+    <section class="w-full cbxh bg-yellow-200 snap-start">
+      Section 5
+    </section>
+    <Global-Footer />
+  </body>
+</template>
+
+<style>
+  .snap {
+    scroll-snap-type: var(--scroll-snap-direction) var(--scroll-snap-constraint);
+  }
+  .snap-y {
+    --scroll-snap-direction: y;
+  }
+  .snap-proximity {
+    --scroll-snap-constraint: proximity;
+  }
+  .snap-start {
+    scroll-snap-align: start;
+  }
+  .snap-padding {
+    scroll-padding-top: 8.33vh;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,13 @@
 <template>
-  <div>
-    <Carousel />
-    <div class="flex flex-col justify-center content-center text-center min-h-screen">
-      <div>
-        <h1>
-          viceroyshayer.github.io
-        </h1>
-        <div>
-          <nuxt-link to="/about">About</nuxt-link>
-          <nuxt-link to="/contact">Contact</nuxt-link>
-          <nuxt-link to="/showcase">showcase</nuxt-link>
-          <nuxt-link to="/blog">blog</nuxt-link>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div></div>
 </template>
 
-<script >
+<script>
   export default {
-    layout: 'canvas',
+    layout: 'home',
   }
 </script>
 
 <style>
-  /*
-  flex flex-col justify-center content-center text-center
-  */
+
 </style>
