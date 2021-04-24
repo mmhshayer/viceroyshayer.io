ec263525c9d9d0302f26c1ee4391a1e04640eda9
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-04-24 14:54:03 +0600

    rework home page

---
+++ layouts/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <body class="cbpt">
+    <Global-Header class="fixed top-0 z-50"/>
+    <Nuxt class="cbmh"/>
+  </body>
+</template>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <img src="https://media.giphy.com/media/f6ytzUt63xVLDDzONe/giphy.gif" alt="fire" class="mx-auto pt-20">
+    <section class="text-xl text-center">
+      <h1>Hi!</h1>
+      <h2>I'm Mohammad Mustakim Hassan</h2>
+      <h2>Welcome to my Site  !</h2>
+    </section>
+  </div>
 </template>
 
 <script>
   export default {
-    layout: 'home',
+    layout: 'index',
   }
 </script>
 
