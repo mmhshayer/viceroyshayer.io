676bc84b6f90d995a625814a386c5890076c87dd
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-05-03 19:56:45 +0600

    sitemap -  hostname

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
 
   // https://www.npmjs.com/package/@nuxtjs/sitemap#installation
   sitemap: {
-    hostname: process.env.BASE_URL,
+    hostname: 'viceroyshayer.github.io',
     exclude: ['/404'],
     routes() {
       return getRoutes();
