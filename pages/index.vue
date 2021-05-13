43b3b15e0dfcd0002fe77a43aacef6907a5757d9
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-05-13 16:33:17 +0600

    game working

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
-    <img src="https://media.giphy.com/media/f6ytzUt63xVLDDzONe/giphy.gif" alt="fire" class="mx-auto pt-20">
-    <section class="text-xl text-center">
-      <h1>Hi!</h1>
-      <h2>I'm Mohammad Mustakim Hassan</h2>
-      <h2>Welcome to my Site  !</h2>
-    </section>
+  <div ref="main" class="min-w-full min-h-screen">
+    <div ref="box" @click="clicked()" class="boxShape">
+      <img src="https://media.giphy.com/media/f6ytzUt63xVLDDzONe/giphy.gif" alt="fire">
+    </div>
+    <div> Score {{ score }}</div>
   </div>
 </template>
 
 <script>
   export default {
-    layout: 'page',
+    layout () {
+      return 'page'
+    },
+    data() {
+      return {
+        score: 0,
+      }
+    },
+    created() {
+      this.interval = setInterval(() => this.teleport(), 2000);
+    },
+    beforeDestroy() {
+      clearInterval(this.interval)
+    },
+
+    methods: {
+      teleport() {
+        const theBox = this.$refs.box
+        theBox.style.left = Math.random() * (this.$refs.main.clientWidth - theBox.clientWidth) + 'px'
+        theBox.style.top = Math.random() * (this.$refs.main.clientHeight - theBox.clientHeight) + 'px'
+      },
+      clicked() {
+        this.score++
+      }
+    }
   }
 </script>
 
 <style>
-
+  .boxShape {
+    width: 2.5rem;
+    height: 4rem;
+    top: -50px;
+    left:  -50px;
+    position: relative;
+    z-index: 50;
+  }
 </style>
+
