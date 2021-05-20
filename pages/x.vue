2ce074185a8f3361ebbbd70dac36921a0ec81291
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-05-20 16:55:27 +0600

    style change on x

--- pages/x.vue
+++ pages/x.vue
@@ -2,7 +2,7 @@
   <div class="bg-yellow-400">
     <div class="min-h-screen min-w-full z-0 grid place-items-center">
       <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-        <h1 class="my-3 text-7xl font-semibold">
+        <h1 class="my-3 text-7xl font-black">
           About
         </h1>
         <p class="leading-relaxed text-lg">Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.</p>
@@ -12,38 +12,50 @@
       </div>
     </div>
 
-    <div class="pb-20 flex flex-col w-full items-center text-center">
+    <div class="pb-20 flex flex-col w-full items-center text-center space-y-20">
 <!-- timeline -->
       <div class="flex flex-col inset-y-5">
         <div>
           <hr class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6">
-          <h1 class="text-3xl font-semibold leading-none">Education</h1>
-          <sub>(time-line)</sub>
+          <h1 class="text-7xl font-extrabold leading-none">Education</h1>
+          <sub>(timeline)</sub>
         </div>
         <div class="pt-10 timeline">
             <ul>
               <li>
                 <div>
-                    <div class="mb-4 mt-2 font-bold leading-3">2019 September</div>
+                    <div class="mb-3 mt-2 font-bold leading-3">2019 - 2023</div>
                     <div class="font-bold text-3xl">Hubei University of Technology</div>
-                    <sub class="italic md:mb-4">Computer Science</sub>
-                    <div class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</div>
+                    <sub class="italic">Wuhan, Hubei, China</sub>
+                    <div class="italic font-semibold mb-3">B.Sc Computer Science</div>
+                    <summary class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</summary>
                 </div>
               </li>
               <li>
                 <div>
-                    <div class="mb-4 mt-2 font-bold leading-3">2019 September</div>
-                    <div class="font-bold text-3xl">Hubei University of Technology</div>
-                    <sub class="italic md:mb-4">Computer Science</sub>
-                    <div class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</div>
+                    <div class="mb-3 mt-2 font-bold leading-3">2016 - 2017</div>
+                    <div class="font-bold text-3xl">Asia Pacific University of Technology & Innovation</div>
+                    <sub class="italic">Technology Park - 5, Kuala Lumpur, Malaysia</sub>
+                    <div class="italic font-semibold mb-3">B.Eng Mechatronics Engineering (dropped out)</div>
+                    <summary class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</summary>
                 </div>
               </li>
               <li>
                 <div>
-                    <div class="mb-4 mt-2 font-bold leading-3">2019 September</div>
-                    <div class="font-bold text-3xl">Hubei University of Technology</div>
-                    <sub class="italic md:mb-4">Computer Science</sub>
-                    <div class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</div>
+                    <div class="mb-3 mt-2 font-bold leading-3">2012 - 2014</div>
+                    <div class="font-bold text-3xl">Notre Dame College</div>
+                    <sub class="italic md:mb-4">Motijheel, Dhaka, Bangladesh</sub>
+                    <div class="italic font-semibold mb-3">HSC &middot; Higher Secondary</div>
+                    <summary class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</summary>
+                </div>
+              </li>
+              <li>
+                <div>
+                    <div class="mb-3 mt-2 font-bold leading-3">2012 - 2010</div>
+                    <div class="font-bold text-3xl">Dhaka Public School</div>
+                    <sub class="italic md:mb-4">Dhanmondi, Dhaka, Bangladesh</sub>
+                    <div class="italic font-semibold mb-3">SSC &middot; Secondary School</div>
+                    <summary class="mb-10">Fusce auctor gravida dui, ut tristique nisi aliquam quis. Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros. Proin nisl felis, consectetur sed elit sed, vestibulum ultrices nibh.</summary>
                 </div>
               </li>
             </ul>
@@ -53,7 +65,7 @@
       <div class="flex flex-col inset-y-5">
         <div>
           <hr class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6">
-          <h1 class="text-3xl font-semibold leading-none">Work Experiance</h1>
+          <h1 class="text-7xl font-extrabold leading-none">Work Experiance</h1>
           <sub>(time-line)</sub>
         </div>
         <div class="pt-10 timeline">
@@ -88,17 +100,17 @@
 <!-- tags -->
       <div>
         <hr class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6">
-        <h1 class="text-3xl font-semibold leading-none">Soft Skills</h1>
+        <h1 class="text-7xl font-extrabold leading-none">Soft Skills</h1>
         <sub>(tag-button)</sub>
       </div>
       <div>
         <hr class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6">
-        <h1 class="text-3xl font-semibold leading-none">Tech Skills</h1>
+        <h1 class="text-7xl font-extrabold leading-none">Tech Skills</h1>
         <sub>(tag-button)</sub>
       </div>
       <div>
         <hr class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6">
-        <h1 class="text-3xl font-semibold leading-none">Tach I've worked with</h1>
+        <h1 class="text-7xl font-extrabold leading-none">Tach I've worked with</h1>
         <sub>(tag-button)</sub>
       </div>
 
@@ -127,19 +139,17 @@
   .timeline ul {
     list-style: none;
   }
-  .timeline ul li {
-    padding: 20px;
-  }
+
   @media only screen and (min-width: 768px) {
     .timeline:before {
       content: "";
       position: absolute;
-      top: 0;
+      top: 1rem;
       left: 50%;
       transform: translateX(-50%);
       width: 2px;
       height: 100%;
-      background-color: gray;
+      background-color: var(--primary-color);
     }
     .timeline ul li {
       width: 50%;
@@ -150,14 +160,12 @@
       clear: right;
       text-align: right;
       transform: translateX(-30px);
-      border-radius: 20px 0px 20px 20px;
     }
     .timeline ul li:nth-child(even) {
       float: right;
       clear: left;
       text-align: left;
       transform: translateX(30px);
-      border-radius: 0px 20px 20px 20px;
     }
     .timeline ul li::before {
       content: "";
@@ -166,7 +174,7 @@
       width: 20px;
       border-radius: 50%;
       background-color: gray;
-      top: 0px;
+      top: 1rem;
     }
     .timeline ul li:nth-child(odd)::before {
       transform: translate(50%, -50%);
