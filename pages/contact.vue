a4d3ce7985eb834201b3ad056af1338ab38ffbcc
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-05-27 11:19:12 +0600

    translated contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -18,33 +18,33 @@
             </div>
             <div class="w-full py-6 z-20">
                 <h1 class="my-3 text-7xl font-semibold bounce-in-top">
-                  {{ $t('contact') }}
+                  {{ $t('mtcontact') }}
                 </h1>
                 <a href="mailto:mustakim13579@mail.com" class="fade-in">{ mustakim13579@gmail.com }</a>
                 <h2 class="text-gray-100 text-2xl fade-in">
-                  or
+                  {{ $t('ctor') }}
                 </h2>
                 <form action="https://formspree.io/f/xdoyaleq" method="POST" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto fade-in">
                   <div class="mb-3">
-                    <label for="name" class="block mb-2 text-sm">Full Name</label>
+                    <label for="name" class="block mb-2 text-sm">{{ $t('ctfullname') }}</label>
                     <input type="text" name="name" id="name" placeholder="John Doe" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                   </div>
                   <div class="mb-3">
-                    <label for="email" class="block mb-2 text-sm">Email Address</label>
+                    <label for="email" class="block mb-2 text-sm">{{ $t('ctemailadd') }}</label>
                     <input type="email" name="email" id="email" placeholder="you@company.com" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                   </div>
                   <div class="mb-3">
 
-                    <label for="phone" class="block mb-2 text-sm">Phone Number</label>
+                    <label for="phone" class="block mb-2 text-sm">{{ $t('ctphoneno') }}</label>
                     <input type="text" name="phone" id="phone" placeholder="+1 (555) 1234-567" required class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" />
                   </div>
                   <div class="mb-3">
-                    <label for="message" class="block mb-2 text-sm">Your Message</label>
+                    <label for="message" class="block mb-2 text-sm">{{ $t('cturmassage') }}</label>
 
                     <textarea rows="5" name="message" id="message" placeholder="Your Message" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" required></textarea>
                   </div>
                   <div class="mb-3">
-                    <button type="submit" class="w-full px-3 py-4 bg-indigo-600 rounded-md focus:bg-indigo-900 focus:outline-none">Send Message</button>
+                    <button type="submit" class="w-full px-3 py-4 bg-indigo-600 rounded-md focus:bg-indigo-900 focus:outline-none">{{ $t('ctsend') }}</button>
                   </div>
                     <p class="text-base text-center" id="result">
                   </p>
