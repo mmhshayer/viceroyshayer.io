c3fff9fa3385f82bec5ce5abb93c6d9426a375f7
Mohammd_Mustakim Hassan <mustakim13579@gmail.com>
2021-05-27 17:37:32 +0600

    ++ fixed wechat broken page

--- pages/wechat.vue
+++ pages/wechat.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="grid place-items-center">
-    <img src="~assets/image/wechat/WeChat_QR_Code.jpg" alt="Scan Code to Add Friend">
+  <div class="h-screen w-full grid place-items-center">
+    <div class="text-center">
+      <img src="~assets/image/wechat/WeChat_QR_Code.jpg" alt="Scan Code to Add Friend">
+      <sub>Hassan_Mo-Jin</sub>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    layout: 'page',
   }
 </script>
 
